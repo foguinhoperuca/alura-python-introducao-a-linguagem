8e25bb1b7c19d181c7c27f701847e483ef728835
Jefferson Campos <jefferson@jeffersoncampos.eti.br>
2025-09-27 10:29:12 -0300

    feat(Python02): Python: Aplicando a Orientação a Objetos.
    Course finished.

--- python-crie-sua-primeira-aplicacao/models/restaurant.py
+++ python-crie-sua-primeira-aplicacao/models/restaurant.py
@@ -13,12 +13,17 @@ class Categories(StrEnum):
 
 
 class Client:
-    def __init__(self: Self, name: str, years: int = 0) -> None:
+    def __init__(self: Self, client_id: int, name: str, years: int = 0) -> None:
+        self._client_id: int = client_id
         self._name: str = name
         self._years: int = years
 
     def __str__(self: Self) -> str:
         return f'Client {self._name} has {self._years} year(s) of loyalty'
+
+    @property
+    def client_id(self: Self) -> int:
+        return self._client_id
 
     @property
     def name(self: Self) -> str:
@@ -28,14 +33,49 @@ class Client:
     def years(self: Self) -> int:
         return self._years
 
+    @classmethod
+    def factory(cls: Self, client_id: int, name: str, years: int) -> Self:
+        if client_id <= 0:
+            raise ValueError(f'ID {id} is invalid!!')
+
+        if name.strip() == '':
+            raise ValueError(f'Name {name} is invalid!!')
+
+        if years < 0:
+            raise ValueError(f'Years {years} is invalid!!')
+
+        return Client(client_id=client_id, name=name, years=years)
+
 
 class Review:
-    def __init__(self, client: Client, grade: int = 1) -> None:
-        self._grade: int = grade
+    MAX_RATING: int = 5
+    MIN_RATING: int = 1
+
+    def __init__(self, client: Client, rating: int = 1) -> None:
+        self._client: Client = client
+        self._rating: int = rating
 
     @property
-    def grade(self: Self) -> int:
-        return self._grade
+    def client(self: Self) -> Client:
+        return self._client
+
+    @property
+    def rating(self: Self) -> int:
+        return self._rating
+
+    @classmethod
+    def validate(cls: Self, client: Client, rating: int) -> bool:
+        is_valid: bool = True
+
+        # TODO validate client
+
+        if not isinstance(rating, int):
+            raise ValueError(f'Invalid rating: {rating}. Only integer values from range {Review.MIN_RATING} (inclusive) to {Review.MAX_RATING} (inclusive)')
+
+        if rating < cls.MIN_RATING or rating > cls.MAX_RATING:
+            is_valid = False
+
+        return is_valid
 
 
 class Restaurant:
@@ -67,34 +107,58 @@ class Restaurant:
     def reviews(self: Self) -> List[Review]:
         return self._reviews
 
+    @reviews.setter
+    def reviews(self: Self, vl: Review) -> None:
+        if Review.validate(vl):
+            self._reviews.append(vl)
+
+        raise ValueError(f'Value {vl} is invalid. No review is added')
+
+    @classmethod
+    def get_category_from_str(cls: Self, cat: str) -> Optional[Categories]:
+        categories: List[str] = [e.value.upper() for e in Categories]
+
+        return Categories(cat) if cat.upper() in categories else None
+
     def is_active(self: Self) -> str:
         return '☑' if self._state else '☐'
 
     def toggle_state(self) -> None:
         self._state = not self._state
 
+    def avarage_rating(self: Self) -> float:
+        """
+        Return avarage rating from all reviews.
+        When no review was made return 0.
+        See reviews to know the range.
+        """
+        if not self._reviews:
+            return 0.0
 
-def restaurant_factory() -> Optional[Restaurant]:
-    try:
-        name: str = input('Inform the name of restaurant:\n')
+        return round(sum(review._rating for review in self._reviews) / len(self._reviews), 2)
 
-        categories: List[str] = [e.value.upper() for e in Categories]
-        while True:
-            cat: str = input('Inform the category of restaurant:\n')
+    @classmethod
+    def factory(cls: Self, name_factory: Optional[str] = None, category_factory: Optional[str] = None) -> Optional[Self]:
+        try:
+            name: str = input('Inform the name of restaurant:\n') if name_factory is None else name_factory
 
-            if cat.upper() in categories:
-                category: Categories = Categories(cat)
-                break
+            categories: List[str] = [e.value.upper() for e in Categories]
+            while True:
+                cat: str = input('Inform the category of restaurant:\n') if category_factory is None else category_factory
 
-            print(f'{cat} not found. Please inform one of follow:')
-            for category in categories:
-                print(f'- {category}')
+                if cat.upper() in categories:
+                    category: Categories = Categories(cat)
+                    break
 
-            sleep(2)
-            print('')
+                print(f'{cat} not found. Please inform one of follow:')
+                for register in categories:
+                    print(f'- {register}')
 
-        return Restaurant(name=name, category=category)
-    except Exception as e:
-        print(f'Could not create music. Error: {e}')
-        sleep(5)
-        return None
+                sleep(2)
+                print('')
+
+            return Restaurant(name=name, category=category)
+        except Exception as e:
+            print(f'Could not create music. Error: {e}')
+            sleep(5)
+            return None
--- python-crie-sua-primeira-aplicacao/second_course.py
+++ python-crie-sua-primeira-aplicacao/second_course.py
@@ -2,11 +2,23 @@ from time import sleep
 from typing import Any, Dict, List
 
 from models.music import Music, music_factory
-from models.restaurant import Restaurant, restaurant_factory
+from models.restaurant import Client, Restaurant, Review
 
 
 musics: List[Music] = []
-restaurants: List[Restaurant] = []
+restaurants: List[Restaurant] = [
+    Restaurant.factory(name_factory='McDonalds', category_factory='Fast Food'),
+    Restaurant.factory(name_factory='Fogo de Chão', category_factory='Brazilian Food'),
+    Restaurant.factory(name_factory='Delícias de Paris', category_factory='French Food'),
+    Restaurant.factory(name_factory='Delícias de Paris', category_factory='French Food'),
+]
+client_portifolio: List[Client] = [
+    Client.factory(client_id=1, name='Mary Adams', years=15),
+    Client.factory(client_id=2, name='Jonh Keys', years=3),
+    Client.factory(client_id=3, name='Mike Sculteler Senior', years=26),
+    Client.factory(client_id=4, name='Mark Wilson Jr', years=7),
+    Client.factory(client_id=5, name='Rose Ann Gutenberg', years=1)
+]
 
 
 def second_course_menu() -> None:
@@ -19,10 +31,11 @@ def second_course_menu() -> None:
 
     print('Enter your option:')
     print('M - create a music')
-    print('R - create a restaurant')
+    print('C - create a restaurant')
     print('S - See restaurant status')
     print('T - Toogle status')
     print('L - List Restaurant')
+    print('R - Review a restaurant')
     print('? - ???')
     print('E - Exit this program')
 
@@ -37,9 +50,8 @@ def second_course_menu() -> None:
                 print(f'List of Musics ({len(musics)} total)')
                 for m in musics:
                     print(m)
-            case 'R':
-                restaurant: Restaurant = restaurant_factory()
-                restaurants.append(restaurant)
+            case 'C':
+                restaurants.append(Restaurant.factory())
                 print(f'List of Restaurants ({len(restaurants)} total)')
                 for m in restaurants:
                     print(m)
@@ -60,11 +72,24 @@ def second_course_menu() -> None:
                     print(f'AFTER...: {restaurant}')
                     break
             case 'L':
-                print(f'{"Name".ljust(30)} | {"Category".ljust(20)} | {"Is Active?".ljust(15)}')
+                print(f'{"Name".ljust(30)} | {"Category".ljust(20)} | {"Is Active?".ljust(15)} | {"Rating".ljust(10)} |')
                 for restaurant in restaurants:
-                    print(f'{restaurant.name.ljust(30)} | {restaurant.category.value.ljust(20)} | {restaurant.is_active().ljust(15)}')
-            case '?':
-                pass
+                    print(f'{restaurant.name.ljust(30)} | {restaurant.category.value.ljust(20)} | {restaurant.is_active().ljust(15)} | {str(restaurant.avarage_rating()).ljust(10)} |')
+            case 'R':
+                while True:
+                    name: str = input("Inform the restaurant's name:\n")
+                    rests: List[Dict[str, Any]] = list(filter(lambda r: r.name == name, restaurants))
+                    if len(rests) == 0:
+                        print(f'Restaurant {name} not found!!')
+                        continue
+
+                    restaurant: Restaurant = rests[0]
+                    restaurant.reviews = Review(client=client_portifolio[0], rating=3)
+                    restaurant.reviews = Review(client=client_portifolio[1], rating=2)
+                    restaurant.reviews = Review(client=client_portifolio[2], rating=5)
+
+                    print(f'Restaurant {restaurant.name} has rating of {restaurant.avarage_rating()}')
+                    break
             case 'E':
                 print('Leaving...')
                 should_exit = True
