f49d52f8e7dd21668dfd695eba662dab05218873
Jefferson Campos <jecampos@sorocaba.sp.gov.br>
2026-03-11 07:42:09 -0300

    feat: Praticando Python.
    Loop: e03

--- training_py/loop.py
+++ training_py/loop.py
@@ -5,11 +5,33 @@ def exerc_01():
 
 
 def exerc_02():
-    print('[LOOP] TODO implement exerc_02')
+    print('[LOOP] exerc_02')
+    counter: int = 0
+    while counter < 10:
+        print(f'[LOOP][[E02]] working... {counter}')
+        counter += 1
 
 
 def exerc_03():
-    print('[LOOP] TODO implement exerc_03')
+    print('[LOOP] exerc_03')
+    counter: int = 1
+    try:
+        total: int = int(input('Inform the total of times to show message: '))
+    except Exception as e:
+        print(f'Using default value: 1 --> {e}')
+        total = 1
+    finally:
+        if total <= 0:
+            total = 1
+
+    while counter <= total:
+        print(f'WHILE: Welcome to ALURA Buscante for {counter}th time!')
+        counter += 1
+
+    print('----------')
+
+    for i in range(total):
+        print(f'FOR: Welcome to ALURA Buscante for {i}th time!')
 
 
 def exerc_04():
