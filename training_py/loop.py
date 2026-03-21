3e287388a8b3460cc337c4708dfd035927dd842c
Jefferson Campos <jefferson@jeffersoncampos.eti.br>
2026-03-21 18:21:55 -0300

    feat: Training Python.
    Loop course finished.

--- training_py/loop.py
+++ training_py/loop.py
@@ -58,20 +58,94 @@ def exerc_05():
 
 
 def exerc_06():
-    print('[LOOP] TODO implement exerc_06')
+    TARGET: str = 'The Hobbit'
+    print('[LOOP] exerc_06')
+    books: List[str] = [
+        '1984',
+        'Dom Casmurro',
+        'The Little Prince',
+        TARGET,
+        'Orgulho e Preconceito',
+        'Memórias Póstumas de Brás Cubas'
+    ]
+    search: str = input('Inform the book name: ')
+
+    for book in books:
+        if search == TARGET:
+            print(f'Book found: {search} !!')
+            break
+
+    if search in books:
+        print(f'FYI: The book {search} searched is in our books collection.')
 
 
 def exerc_07():
-    print('[LOOP] TODO implement exerc_07')
+    INVENTORY: int = 5
+    print('[LOOP] exerc_07')
+    for item in range(INVENTORY):
+        leftover = INVENTORY - (item + 1)
+        if leftover >= 0:
+            print(f'[FOR] Selling done! [item: {item}] Still in inventory: {leftover}')
+
+    print('-----')
+    stock: int = INVENTORY
+    while stock > 0:
+        stock -= 1
+        print(f'[WHILE] Selling is done! Still in stock: {stock}')
+
+    print('')
+    print('Inventory/Stock is over!!')
 
 
 def exerc_08():
-    print('[LOOP] TODO implement exerc_08')
+    COUNTDOWN: int = 10
+    print('[LOOP] exerc_08')
+    for final_countdown in range(COUNTDOWN, 0, -1):
+        if final_countdown % 2 == 0:
+            print(f"Only {final_countdown} left - don't miss the promotion!!")
+        else:
+            print(f"The countdown is going on: only {final_countdown} seconds.")
+
+    print('Go ahead and enjoy your promotion!!')
 
 
 def exerc_09():
-    print('[LOOP] TODO implement exerc_09')
+    print('[LOOP] exerc_09')
+
+    books: List[str] = [
+        {"name": "1984", "stock": 5},
+        {"name": "Dom Casmurro", "stock": 0},
+        {"name": "The Little Prince", "stock": 3},
+        {"name": "The Hobbit", "stock": 0},
+        {"name": "Orgulho e Preconceito", "stock": 2},
+        {"name": "Memórias Póstumas de Brás Cubas", "stock": 7}
+    ]
+
+    for book in books:
+        if book['stock'] == 0:
+            continue
+
+        print(f'Book still available (stock: {book["stock"]}) :: {book["name"]}')
+        # else:
+        #     print(f'Book *** MISSING *** (stock: {book["stock"]}) :: {book["name"]}')
 
 
 def exerc_10():
-    print('[LOOP] TODO implement exerc_10')
+    USERNAME_MIN_LENGHT: int = 5
+    PASSWORD_MIN_LENGHT: int = 8
+    print('[LOOP] exerc_10')
+
+    while True:
+        username: str = input('Inform your username..: ')
+        password: str = input('Inform your password..: ')
+
+        if len(username) < USERNAME_MIN_LENGHT:
+            print(f'Your username {username} is invalid: it should contain at least {USERNAME_MIN_LENGHT} character (has {len(username)} characters)!!')
+            continue
+
+        if len(password) < PASSWORD_MIN_LENGHT:
+            print(f'Your password {password} is invalid: it should contain at least {PASSWORD_MIN_LENGHT} character (has {len(password)} characters)!!')
+            continue
+
+        print(f'Username: {username} and password: *** successfully registered!!')
+        break
